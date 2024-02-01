cdf4b5be03de7dcae3fdde722fe16c8f130dbac9
Dara Schuindt <137013013+daraschuindt@users.noreply.github.com>
2024-01-31 21:40:20 -0500

    Add files via upload

---
+++ general.js
@@ -0,0 +1,25 @@
+function myFunction() {
+  var x = document.getElementById("myLinks");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
+
+let mybutton = document.getElementById("myBtn");
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 1200 || document.documentElement.scrollTop > 1200) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+function topFunction() {
+  document.body.scrollTop = 0; 
+  document.documentElement.scrollTop = 0;
+}
